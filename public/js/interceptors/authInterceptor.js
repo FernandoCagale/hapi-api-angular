7de9181b3eb6149632ce76fa6e2524194b50bc35
Fernando Cagale <fernando.b.cagale@gmail.com>
2015-05-26 13:03:17 -0300

    Add

--- public/js/interceptors/authInterceptor.js
+++ public/js/interceptors/authInterceptor.js
@@ -6,8 +6,6 @@ angular.module("app").factory('authInterceptor', function($q, $location) {
 
             var token = $.jStorage.get('authorization');
 
-            console.log(config.url)
-
             if (config.url == 'partials/auth.html')
               $.jStorage.set('authorization', null);
 
